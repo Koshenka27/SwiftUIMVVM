644571543f7cc5ec48c29aef50f446c24a240748
Logan Koshenka <koshenka27@gmail.com>
2021-10-23 13:57:22 -0400

    Initial Commit

---
+++ swiftuiMVVM/swiftuiMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiMVVMApp.swift
+//  swiftuiMVVM
+//
+//  Created by Logan Koshenka on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
